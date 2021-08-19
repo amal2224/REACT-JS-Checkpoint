b03a7196aa1f7212194eabfd473b383a4f78fcea
amal2224 <kthiri.amal20@gmail.com>
2021-08-19 17:11:12 +0100

    this's my first app with react , in this checkpoint i just adda bootstrap to myt app, and create a form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import {Navbar,Container,Nav,Form,Button} from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<>
+{/* navbar bootstarp */}
+<Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Social media</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Profil</Nav.Link>
+      <Nav.Link href="#features">Friends</Nav.Link>
+      <Nav.Link href="#pricing">Events</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+  {/* form */}
+  <div>
+    <Container className="myForm">
+      <h1 className="shadow-sm text-success mt-5 p-3 text-center rounded">Login</h1>
+<Container className="myform">
+    <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label className="labelText">Email | Phone | UserName</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className="labelText">Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" className="labelText2"/>
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Log In
+  </Button>
+</Form>
+</Container>
+</Container>
+  </div>
+</>  );
 }
 
 export default App;
